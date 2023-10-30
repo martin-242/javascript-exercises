cbfd4e35804beffb4815850932f6fd4fc3ea4a7a
martin-242 <martin242@gmail.com>
2023-10-30 19:00:50 +0100

    Solves exercises 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    let conversion = (fahrenheit - 32) * (5/9);
+    if ( conversion % 1 === 0 ) { 
+    return conversion;
+  }
+  else {
+    let rounded = Math.round(conversion * 10) / 10;
+    return rounded;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let conversion = (celsius * (9/5) + 32);
+  if ( conversion % 1 === 0 ) { 
+  return conversion;
+}
+else {
+  let rounded = Math.round(conversion * 10) / 10;
+  return rounded;
+}
 };
 
 // Do not edit below this line
