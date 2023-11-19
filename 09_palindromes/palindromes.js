5c5c5787b8d9613d7298f7000d79a7360e9d5b1f
martin-242 <martin242@gmail.com>
2023-11-19 20:37:47 +0100

    Solves exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
-};
+const palindromes = function (input) {
+    let string = input.trim().toLowerCase().replace(/\W/g, '');
+      let length = string.length -1;
+      for (i=0; i < length; i++){
+          let charA = string.charAt(i);
+          let charB = "";
+          if (i == 0) {
+            charB = string.charAt(length);
+            }
+          else{
+            charB = string.charAt(length -i);
+          }
+          if ( charA != charB ) {
+              return false;
+          }
+      }
+      return true;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
